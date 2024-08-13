8a91c66e9ed1c43be592d5bac3ec02c3e57a7872
Michael Ladderbush <38595703+mikeladderbush@users.noreply.github.com>
2024-08-13 12:14:34 -0400

    Update ApplicationConfiguration.java
    updated postgresql authentication and userdetails

--- src/main/java/com/BJJ/BJJSite/Config/ApplicationConfiguration.java
+++ src/main/java/com/BJJ/BJJSite/Config/ApplicationConfiguration.java
@@ -10,16 +10,30 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class ApplicationConfiguration implements WebMvcConfigurer {
 
+    @Autowired
+    DataSource dataSource;
+    
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((requests) -> requests.anyRequest().authenticated())
-                .httpBasic(Customizer.withDefaults())
-                .formLogin(Customizer.withDefaults());
-
+        http.authorizeHttpRequests((requests) -> 
+            requests.requestMatchers("/h2-console/**").permitAll().anyRequest().authenticated());
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.httpBasic(withDefaults());
+        http.headers(headers ->
+                    headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+        http.csrf(AbstractHttpConfigurer::disable);
         return http.build();
+    }
 
+    @Bean
+    public UserDetailsService userDetailsService(){
+
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        //Create users here until API is set up
+        userDetailsManager.createUser();
+        return userDetailsManager;
     }
 }
