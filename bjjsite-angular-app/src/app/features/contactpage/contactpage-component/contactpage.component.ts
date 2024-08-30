9e188334be8fea38e67b91a95c5a1587ea6d3bb5
Michael Ladderbush <mikeladderbush@gmail.com>
2024-08-29 23:14:02 -0400

    Adding default look for pages

--- bjjsite-angular-app/src/app/features/contactpage/contactpage-component/contactpage.component.ts
+++ bjjsite-angular-app/src/app/features/contactpage/contactpage-component/contactpage.component.ts
@@ -1,21 +1,53 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 /**
  * ContactpageComponent
  * 
  * This component represents the "Contact" page of the application.
  * 
- * - The `templateUrl` points to the HTML file that defines the component's view.
- * - The `styleUrl` points to the CSS file that defines the component's styles.
+ * - The `templateUrl` points to the HTML file defining the component's view.
+ * - The `styleUrl` points to the CSS file defining the component's styles.
  * - The `selector` allows this component to be used as `<app-contactpage>` in the application's HTML.
  */
 @Component({
-  selector: 'app-contactpage', // Custom HTML tag for the "Contact" page component
+  selector: 'about', // Custom HTML tag for the "About" page component
   standalone: true, // Indicates that this component is standalone
-  imports: [], // No external modules are currently imported
+  imports: [CommonModule, RouterModule], // No external modules are currently imported
   templateUrl: './contactpage.component.html', // Path to the HTML template
   styleUrl: './contactpage.component.css' // Path to the CSS file
 })
-export class ContactpageComponent {
-  // The class is currently empty, but it can be expanded to include component logic
+export class ContactpageComponent implements OnInit {
+  user: any;
+
+  constructor(private http: HttpClient, private cdr: ChangeDetectorRef) { }
+  ngOnInit(): void {
+    this.getUser(1);
+  }
+
+  /**
+   * getUser
+   * 
+   * Fetches user data from the backend API based on the provided user ID.
+   * 
+   * @param id - The ID of the user to fetch
+   */
+  getUser(id: number): void {
+    this.http.get(`http://localhost:8080/api/users/${id}`)
+      .subscribe({
+        next: data => {
+          this.user = data;
+          console.log(this.user);
+          this.cdr.detectChanges(); // Manually triggers change detection
+        },
+        error: error => {
+          console.error('There was an issue getting a user', error);
+        },
+        complete: () => {
+          console.log('Request complete');
+        }
+      });
+  }
 }
